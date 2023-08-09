551d4b5ea2cc7b38da329d7b498853b5775e15e5
Jesper Kristensen <cableman@linuxdev.dk>
2023-08-09 12:06:57 +0200

    Updated module to match ding2

--- js/itk-siteimprove.js
+++ js/itk-siteimprove.js
@@ -8,18 +8,27 @@
 
   Drupal.behaviors.itkSiteimprove = {
     attach: function (context, settings) {
-      var key = settings.itk_siteimprove.key;
 
-      if (key) {
-        (function () {
-          var sz = document.createElement('script');
-          sz.type = 'text/javascript';
-          sz.async = true;
-          sz.src = '//ssl.siteimprove.com/js/siteanalyze_' + key + '.js';
-          var s = document.getElementsByTagName('script')[0];
-          s.parentNode.insertBefore(sz, s);
-        })();
-      }
+      $('body').once('siteimprove-processed', function() {
+        const key = settings.itk_siteimprove.key;
+        if (key) {
+          window.addEventListener('CookieInformationConsentGiven', function (event) {
+            if (CookieInformation.getConsentGivenFor('cookie_cat_statistic')) {
+              (function()
+                {
+                  const sz = document.createElement('script');
+                  sz.type = 'text/javascript';
+                  sz.async = true;
+                  sz.src = '//siteimproveanalytics.com/js/siteanalyze_' + key + '.js';
+
+                  const s = document.getElementsByTagName('script')[0];
+                  s.parentNode.insertBefore(sz, s);
+                }
+              )();
+            }
+          }, false);
+        }
+      });
     }
   };
 })(jQuery, Drupal);
